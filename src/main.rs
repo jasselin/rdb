81a2da23e5ca47d22e2c96e9e03b3def62665e1f
Jason Asselin <asselin.jason@gmail.com>
2019-04-06 14:29:31 -0400

    Part 1 - Introduction and Setting up the REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    ::std::process::exit(match run_app() {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("error: {:?}", err);
+            1
+        }
+    });
+}
+
+fn run_app() -> Result<(), ()> {
+    loop {
+        print_prompt();
+        let input = read_input();
+
+        if input == ".exit" {
+            return Ok(());
+        } else {
+            println!("Unrecognized command '{}'.", input)
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("db > ");
+    io::stdout().flush().unwrap();
+}
+
+fn read_input() -> String {
+    let stdin = io::stdin();
+    let mut iterator = stdin.lock().lines();
+    iterator.next().unwrap().unwrap()
 }
